aa8330cfede3f56b9804d00f06d5e1a6546b50f4
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-11 17:15:06 -0600

    classes and methods testing basics

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Cat{
+    constructor(nm, gdr)
+    {
+        this.name = nm;
+        this.gender = gdr;
+    }
+
+    speak(){
+        return "" + this.name + " says meow!";
+    }
+}
+
+class Dog{
+    constructor(nm, gdr)
+    {
+        this.name = nm;
+        this.gender = gdr;
+    }
+
+    speak(){
+        return "" + this.name + " says woof!";
+    }
+}
+
+class Bird{
+    constructor(nm, gdr)
+    {
+        this.name = nm;
+        this.gender = gdr;
+    }
+
+    speak(){
+        if (this.gender === "male") return "It's me! " + this.name + ", the parrot!";
+        else return "" + this.name + " says squawk!";
+    }
+}
